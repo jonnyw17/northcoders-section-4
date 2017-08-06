4693bae12ed33616613c62532aed0375b60fba54
jonnyw17 <jonny@spaceportx.com>
2017-08-06 22:45:00 +0100

    3/4 challenges completed

--- 2-underscoreMethods/main.js
+++ 2-underscoreMethods/main.js
@@ -1,13 +1,53 @@
 function first (array, n) {
 
-}
-.
-function rest (array, index) {
+	var arrJoin = array.join(' ');
+	var firstNum = [];
 
+	if(arguments.length === 1){
+		for(var i = 0; i < 1; i++){
+			firstNum.push(array.shift());
+		}
+		firstNum = firstNum.join(' ');
+		return firstNum;
+
+	}else if(n < 2){
+		for(var y = 0; y < n; y++){
+			firstNum.push(array.shift());
+		}
+		firstNum = firstNum.join(' ');
+		firstNum = Number(firstNum);
+
+		return firstNum;
+	}else {
+		for(var x = 0; x < n; x++){
+			firstNum.push(array.shift());
+		}
+	}
+		return firstNum;
+	}
+
+
+function rest (array, index) {
+	if(arguments.length > 1){
+	   array.splice(0,index);
+  }else {
+     array.shift();
+  }
+	return array
 }
 
 function contains (array, target, fromIndex) {
 
+var answer;
+
+    for(var i = 0; i < array.length; i++){
+      if(array[i] === target){
+        answer = true;
+      }else{
+        answer = false;
+      }
+    }
+    return answer
 }
 
 function compact (array) {
